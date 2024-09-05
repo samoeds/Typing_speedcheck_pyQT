1132ed59123f29f789966631ffc890e1f22812d7
Elena <trndzen@gmail.com>
2024-09-04 19:35:26 -0700

    start button added, restart score and timer function added, best score added

--- main.py
+++ main.py
@@ -15,22 +15,34 @@ class Ui(QtWidgets.QMainWindow):
         self.time_left = 61
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_timer)
-        self.start_timer()
 
+        # BEST SCORE
         self.your_best_score = self.findChild(QtWidgets.QLabel, 'your_best_score')
         self.best_score = 0
         self.your_best_score.setText(str(self.best_score))
 
+        # CPM
         self.your_cpm = self.findChild(QtWidgets.QLabel, 'CPM')
         self.cpm_score = 0
         self.your_cpm.setText(str(self.cpm_score))
 
+        # WPM
         self.your_wpm = self.findChild(QtWidgets.QLabel, 'WPM')
         self.wpm_score = 0
         self.your_wpm.setText(str(self.wpm_score))
 
+        # RESTART
         self.restart_label = self.findChild(QtWidgets.QPushButton, "restart_button")
         self.restart_label.clicked.connect(self.restart)
+
+        # START
+        self.start_label = self.findChild(QtWidgets.QPushButton, "start_button")
+        self.start_label.clicked.connect(self.start_button_activated)
+
+        # TEXT_ENTRY
+        self.text_entry = self.findChild(QtWidgets.QTextEdit, "text_entry")
+        self.text_entry.textChanged.connect(self.your_best)
+
 
 
     def start_timer(self):
@@ -45,10 +57,18 @@ class Ui(QtWidgets.QMainWindow):
             self.timer.stop()
 
     def restart(self):
-        self.time_left = 61
+        self.text_entry.clear()
+        self.time_left = 60
+
+
+    def start_button_activated(self):
+        self.start_timer()
+
 
     def your_best(self):
-        pass
+        text = []
+        text = self.text_entry.toPlainText().split()
+        print(len(text))
 
     def wpm(self):
         pass
@@ -63,8 +83,9 @@ def main():
     app = QtWidgets.QApplication(sys.argv)
     window = Ui()
     window.setWindowTitle("Typing speed test")
-    window.setFixedSize(600, 447)
+    window.setFixedSize(600, 483)
     window.show()
+
     app.exec()
 
 
